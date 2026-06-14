39120ea4e298d0c32b2de7d95fac4031c029f8ac
Gustavo Donato <gugaodonato@gmail.com>
2026-06-14 19:04:29 -0300

    feat: adiciona modal_termos_de_uso ao iniciar a aplicacao

--- src/database/database.py
+++ src/database/database.py
@@ -4,7 +4,7 @@ conexao = mysql.connector.connect(
     host="mysql-34bfce49-poo-pi.c.aivencloud.com",
     port="17819",
     user="avnadmin",
-    password="",
+    password="AVNS_7mkGmMRCU5OtlEqgiMB",
     database="domino_quimico",
     ssl_disabled=False
 )
--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 import sys
+import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "src")
+    )
+)
+
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QPalette, QColor
 
 from ui.screens.tela_login import TelaLogin
+from ui.modals.modal_termos_de_uso import TermosDeUso
 from controllers.controller_tela_login import ControllerTelaLogin
 
 
@@ -14,10 +23,16 @@ def main():
     palette.setColor(QPalette.Window, QColor("white"))
     app.setPalette(palette)
 
-
+    # Abre a tela de login primeiro
     tela_login = TelaLogin()
     controller = ControllerTelaLogin(tela_login)
     tela_login.show()
+
+    termos = TermosDeUso(parent=tela_login)
+    resultado = termos.exec()
+
+    if resultado != TermosDeUso.Accepted:
+        sys.exit(0)
 
     sys.exit(app.exec())
 
